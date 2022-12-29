2bdb1a847d972caf2b9a1bb762c756196ec0cc53
ookuboc5399 <ankle0614@gmail.com>
2022-12-30 01:58:42 +0900

    1230

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import { getSession, useSession } from "next-auth/react";
+import Center from "../components/Center";
+import Player from "../components/Player";
+import Sidebar from "../components/Sidebar";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="overflow-hidden scrollbar-hide flex">
+        <Sidebar />
+        <Center />
+      </main>
+
+      <div className="sticky bottom-0">
+        <Player />
+      </div>
+    </div>
+  )
+}
+
+export default Home
